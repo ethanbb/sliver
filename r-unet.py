45f686a85fb186edfc574bb2ccbc6d52af34a6b4
Ethan Blackwood <ethan@cs.dartmouth.edu>
2017-05-10 12:41:25 -0400

    Start hooking up LSTM with Unet

---
+++ r-unet.py
@@ -0,0 +1,170 @@
+from __future__ import print_function, division, absolute_import
+from collections import OrderedDict
+from tf_unet_1 import unet
+
+
+class RUnet(unet.Unet):
+    """Model that incorporates a Unet and a bidirectional convolutional LSTM
+    n_lstm_layers is the number of vertical bidirectional LSTM layers
+    All other inputs are the same as the inputs to Unet.__init__.
+    """
+    def __init__(self, n_lstm_layers=1, channels=3, n_class=2,
+                 cost="cross_entropy", cost_kwargs={}, **kwargs):
+        tf.reset_default_graph()
+
+        self.n_class = n_class
+        self.summaries = kwargs.get("summaries", True)
+
+        self.x = tf.placeholder("float", shape=[None, None, None, channels])
+        self.y = tf.placeholder("float", shape=[None, None, None, n_class])
+        self.keep_prob = tf.placeholder(tf.float32)  # dropout keep probability
+
+        # set up Unet
+        feature_maps, unet_logits, self.variables, self.offset = create_conv_net(
+            self.x, self.keep_prob, channels, n_class, **kwargs)
+
+        self._unet_cost = self._get_cost(unet_logits, cost, cost_kwargs)
+
+        # To be continued...
+
+def create_conv_net(x, keep_prob, channels, n_class, layers=3, features_root=16,
+                    filter_size=3, pool_size=2, summaries=True):
+    """
+    Creates a new convolutional unet for the given parametrization.
+
+    :param x: input tensor, shape [?,nx,ny,channels]
+    :param keep_prob: dropout probability tensor
+    :param channels: number of channels in the input image
+    :param n_class: number of output labels
+    :param layers: number of layers in the net
+    :param features_root: number of features in the first layer
+    :param filter_size: size of the convolution filter
+    :param pool_size: size of the max pooling operation
+    :param summaries: Flag if summaries should be created
+    """
+
+    logging.info("Layers {layers}, features {features}, "
+                 "filter size {filter_size}x{filter_size}, "
+                 "pool size: {pool_size}x{pool_size}".format(
+                    layers=layers, features=features_root,
+                    filter_size=filter_size, pool_size=pool_size))
+
+    # Placeholder for the input image
+    nx = tf.shape(x)[1]
+    ny = tf.shape(x)[2]
+    x_image = tf.reshape(x, tf.stack([-1, nx, ny, channels]))
+    in_node = x_image
+    batch_size = tf.shape(x_image)[0]
+
+    weights = []
+    biases = []
+    convs = []
+    pools = OrderedDict()
+    deconv = OrderedDict()
+    dw_h_convs = OrderedDict()
+    up_h_convs = OrderedDict()
+
+    in_size = 1000
+    size = in_size
+    # down layers
+    for layer in range(0, layers):
+        features = 2**layer*features_root
+        stddev = np.sqrt(2 / (filter_size**2 * features))
+        if layer == 0:
+            w1 = weight_variable([filter_size, filter_size,
+                                 channels, features], stddev)
+        else:
+            w1 = weight_variable([filter_size, filter_size,
+                                 features//2, features], stddev)
+
+        w2 = weight_variable([filter_size, filter_size,
+                             features, features], stddev)
+        b1 = bias_variable([features])
+        b2 = bias_variable([features])
+
+        conv1 = conv2d(in_node, w1, keep_prob)
+        tmp_h_conv = tf.nn.relu(conv1 + b1)
+        conv2 = conv2d(tmp_h_conv, w2, keep_prob)
+        dw_h_convs[layer] = tf.nn.relu(conv2 + b2)
+
+        weights.append((w1, w2))
+        biases.append((b1, b2))
+        convs.append((conv1, conv2))
+
+        size -= 4
+        if layer < layers-1:
+            pools[layer] = max_pool(dw_h_convs[layer], pool_size)
+            in_node = pools[layer]
+            size /= 2
+
+    in_node = dw_h_convs[layers-1]
+
+    # up layers
+    for layer in range(layers-2, -1, -1):
+        features = 2**(layer+1)*features_root
+        stddev = np.sqrt(2 / (filter_size**2 * features))
+
+        wd = weight_variable_devonc([pool_size, pool_size,
+                                    features//2, features], stddev)
+        bd = bias_variable([features//2])
+        h_deconv = tf.nn.relu(deconv2d(in_node, wd, pool_size) + bd)
+        h_deconv_concat = crop_and_concat(dw_h_convs[layer], h_deconv)
+        deconv[layer] = h_deconv_concat
+
+        w1 = weight_variable([filter_size, filter_size,
+                             features, features//2], stddev)
+        w2 = weight_variable([filter_size, filter_size,
+                             features//2, features//2], stddev)
+        b1 = bias_variable([features//2])
+        b2 = bias_variable([features//2])
+
+        conv1 = conv2d(h_deconv_concat, w1, keep_prob)
+        h_conv = tf.nn.relu(conv1 + b1)
+        conv2 = conv2d(h_conv, w2, keep_prob)
+        in_node = tf.nn.relu(conv2 + b2)
+        up_h_convs[layer] = in_node
+
+        weights.append((w1, w2))
+        biases.append((b1, b2))
+        convs.append((conv1, conv2))
+
+        size *= 2
+        size -= 4
+
+    # output before mapping to n_class for connection to LSTM
+    output_raw = in_node
+
+    # Output Map
+    weight = weight_variable([1, 1, features_root, n_class], stddev)
+    bias = bias_variable([n_class])
+    conv = conv2d(in_node, weight, tf.constant(1.0))
+    output_map = tf.nn.relu(conv + bias)
+    up_h_convs["out"] = output_map
+
+    if summaries:
+        for i, (c1, c2) in enumerate(convs):
+            tf.summary.image('summary_conv_%02d_01' % i, get_image_summary(c1))
+            tf.summary.image('summary_conv_%02d_02' % i, get_image_summary(c2))
+
+        for k in pools.keys():
+            tf.summary.image('summary_pool_%02d' % k, get_image_summary(pools[k]))
+
+        for k in deconv.keys():
+            tf.summary.image('summary_deconv_concat_%02d' % k, get_image_summary(deconv[k]))
+
+        for k in dw_h_convs.keys():
+            tf.summary.histogram("dw_convolution_%02d" % k + '/activations', dw_h_convs[k])
+
+        for k in up_h_convs.keys():
+            tf.summary.histogram("up_convolution_%s" % k + '/activations', up_h_convs[k])
+
+    variables = []
+    for w1, w2 in weights:
+        variables.append(w1)
+        variables.append(w2)
+
+    for b1, b2 in biases:
+        variables.append(b1)
+        variables.append(b2)
+
+    return output_raw, output_map, variables, int(in_size - size)

07572701a036f8e17f95d1ab5f5a8b70788276be
Angela Zhu <zhu.angela17@gmail.com>
2017-05-17 19:59:40 -0400

    File hoping to load in a trained model and test on the batch 2 of liver scans. Not currently functioning

---
+++ test_launcher.py
@@ -0,0 +1,37 @@
+from __future__ import print_function, division, absolute_import, unicode_literals
+from tf_unet_1 import unet
+from tf_unet_1 import util
+from data_gen import *
+
+if __name__ == '__main__':
+    training_iters = 20
+    epochs = 10
+    dropout = 0.75  # Dropout, probability to keep units
+    display_step = 2
+    restore = False
+
+    generator = CTScanTrainDataProvider('/ihome/azhu/cs189/data/liverScans/Training Batch 1/npy_data_notoken/')
+    test_generator = CTScanTestDataProvider('/ihome/azhu/cs189/data/liverScans/Training Batch 2/npy_data_notoken/')
+
+    batch_size = 4
+
+    net = unet.Unet(channels=generator.channels,
+                    n_class=generator.n_class,
+                    layers=3,
+                    features_root=16,
+                    cost="dice_coefficient")
+
+    trainer = unet.Trainer(net, batch_size=batch_size, optimizer="momentum",
+                           opt_kwargs=dict(momentum=0.2, learning_rate=0.2))
+
+    path = trainer.train(generator, "./unet_trained",
+                         training_iters=training_iters,
+                         epochs=epochs,
+                         dropout=dropout,
+                         display_step=display_step,
+                         restore=restore)
+
+    x_test, y_test = test_generator(4)
+    prediction = net.predict(path, x_test)
+
+    print("Testing error rate: {:.2f}%".format(unet.error_rate(prediction, util.crop_to_shape(y_test, prediction.shape))))

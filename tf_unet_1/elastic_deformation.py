5c5a8ff94dab8d63499b85f4d80d554822952d9b
Angela Zhu <zhu.angela17@gmail.com>
2017-05-22 20:33:46 -0400

    Account for multiple label arrays

--- tf_unet_1/elastic_deformation.py
+++ tf_unet_1/elastic_deformation.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.ndimage.interpolation import map_coordinates
 from scipy.ndimage.filters import gaussian_filter
 
-def elastic_transform(image, label, alpha, sigma, random_state=None):
+def elastic_transform(image, labels, alpha, sigma, random_state=None):
     """Elastic deformation of images as described in [Simard2003]_.
     .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
        Convolutional Neural Networks applied to Visual Document Analysis", in
@@ -13,6 +13,10 @@ def elastic_transform(image, label, alpha, sigma, random_state=None):
            https://gist.github.com/chsasank/4d8f68caf01f041a6453e67fb30f8f5a
     """
     assert len(image.shape) == 2
+
+    label0 = labels[..., 0]
+    label1 = labels[..., 1]
+    label2 = labels[..., 2]
 
     if random_state is None:
         random_state = np.random.RandomState(None)
@@ -26,5 +30,12 @@ def elastic_transform(image, label, alpha, sigma, random_state=None):
     indices = np.reshape(x+dx, (-1, 1)), np.reshape(y+dy, (-1, 1))
 
     image_mapped = map_coordinates(image, indices, order=1).reshape(shape)
-    label_mapped = map_coordinates(label, indices, order=1).reshape(shape)
-    return image_mapped, label_mapped
+    label0_mapped = map_coordinates(label0, indices, order=1).reshape(shape)
+    label1_mapped = map_coordinates(label1, indices, order=1).reshape(shape)
+    label2_mapped = map_coordinates(label2, indices, order=1).reshape(shape)
+
+    labels[..., 0] = label0_mapped
+    labels[..., 1] = label1_mapped
+    labels[..., 2] = label2_mapped
+
+    return image_mapped, labels
